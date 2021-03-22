59c005484ca4ed190a147e0cf6a517a49705c646
John Lowe <jblowe@berkeley.edu>
2021-03-22 16:14:43 -0700

    nahc-public core specific revisionts

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -65,10 +65,43 @@ class CatalogController < ApplicationController
     config.add_nav_action(:bookmark, partial: 'blacklight/nav/bookmark', if: :render_bookmarks_control?)
     config.add_nav_action(:search_history, partial: 'blacklight/nav/search_history')
 
+    # NAHC customizations to support existing Solr cores
+    config.default_solr_params = {
+      rows: 10,
+      'facet.mincount': 1,
+      'q.alt': '*:*',
+      'defType': 'edismax',
+      'df': 'text',
+      'q.op': 'AND',
+      'q.fl': '*,score'
+    }
+
+    # solr path which will be added to solr base url before the other solr params.
+    #config.solr_path = 'select'
+
+    # items to show per page, each number in the array represent another option to choose from.
+    #config.per_page = [10,20,50,100]
+
+    ## Default parameters to send on single-document requests to Solr. These settings are the Blackligt defaults (see SearchHelper#solr_doc_params) or
+    ## parameters included in the Blacklight-jetty document requestHandler.
+    #
+    config.default_document_solr_params = {
+      qt: 'document',
+    #  ## These are hard-coded in the blacklight 'document' requestHandler
+    #  # fl: '*',
+    #  # rows: 1,
+    # NAHC customization: this is needed for our Solr4 services
+      q: '{!term f=id v=$id}'
+    }
+
+    # solr field configuration for search results/index views
+    # NAHC customization: list of blobs is hardcoded for both index and show displays
+    #{index_title}
+    #config.index.thumbnail_field = 'blob_ss'
+
     # solr field configuration for document/show views
-    #config.show.title_field = 'title_tsim'
-    #config.show.display_type_field = 'format'
-    #config.show.thumbnail_field = 'thumbnail_path_ss'
+    #{show_title}
+    #config.show.thumbnail_field = 'blob_ss'
 
     # solr fields that will be treated as facets by the blacklight application
     #   The ordering of the field names is the order of the display
@@ -94,21 +127,12 @@ class CatalogController < ApplicationController
     #  (useful when user clicks "more" on a large facet and wants to navigate alphabetically across a large set of results)
     # :index_range can be an array or range of prefixes that will be used to create the navigation (note: It is case sensitive when searching values)
 
-    config.add_facet_field 'format', label: 'Format'
-    config.add_facet_field 'pub_date_ssim', label: 'Publication Year', single: true
-    config.add_facet_field 'subject_ssim', label: 'Topic', limit: 20, index_range: 'A'..'Z'
-    config.add_facet_field 'language_ssim', label: 'Language', limit: true
-    config.add_facet_field 'lc_1letter_ssim', label: 'Call Number'
-    config.add_facet_field 'subject_geo_ssim', label: 'Region'
-    config.add_facet_field 'subject_era_ssim', label: 'Era'
 
-    config.add_facet_field 'example_pivot_field', label: 'Pivot Field', pivot: ['format', 'language_ssim'], collapsing: true
-
-    config.add_facet_field 'example_query_facet_field', label: 'Publish Date', :query => {
-       :years_5 => { label: 'within 5 Years', fq: "pub_date_ssim:[#{Time.zone.now.year - 5 } TO *]" },
-       :years_10 => { label: 'within 10 Years', fq: "pub_date_ssim:[#{Time.zone.now.year - 10 } TO *]" },
-       :years_25 => { label: 'within 25 Years', fq: "pub_date_ssim:[#{Time.zone.now.year - 25 } TO *]" }
-    }
+    #config.add_facet_field 'example_query_facet_field', label: 'Publish Date', :query => {
+    #   :years_5 => { label: 'within 5 Years', fq: "pub_date:[#{Time.zone.now.year - 5 } TO *]" },
+    #   :years_10 => { label: 'within 10 Years', fq: "pub_date:[#{Time.zone.now.year - 10 } TO *]" },
+    #   :years_25 => { label: 'within 25 Years', fq: "pub_date:[#{Time.zone.now.year - 25 } TO *]" }
+    #}
 
 
     # Have BL send all facet field names to Solr, which has been the default
@@ -118,32 +142,14 @@ class CatalogController < ApplicationController
 
     # solr fields to be displayed in the index (search results) view
     #   The ordering of the field names is the order of the display
-    config.add_index_field 'title_tsim', label: 'Title'
-    config.add_index_field 'title_vern_ssim', label: 'Title'
-    config.add_index_field 'author_tsim', label: 'Author'
-    config.add_index_field 'author_vern_ssim', label: 'Author'
-    config.add_index_field 'format', label: 'Format'
-    config.add_index_field 'language_ssim', label: 'Language'
-    config.add_index_field 'published_ssim', label: 'Published'
-    config.add_index_field 'published_vern_ssim', label: 'Published'
-    config.add_index_field 'lc_callnum_ssim', label: 'Call number'
+
+    #{index}
 
     # solr fields to be displayed in the show (single result) view
     #   The ordering of the field names is the order of the display
-    config.add_show_field 'title_tsim', label: 'Title'
-    config.add_show_field 'title_vern_ssim', label: 'Title'
-    config.add_show_field 'subtitle_tsim', label: 'Subtitle'
-    config.add_show_field 'subtitle_vern_ssim', label: 'Subtitle'
-    config.add_show_field 'author_tsim', label: 'Author'
-    config.add_show_field 'author_vern_ssim', label: 'Author'
-    config.add_show_field 'format', label: 'Format'
-    config.add_show_field 'url_fulltext_ssim', label: 'URL'
-    config.add_show_field 'url_suppl_ssim', label: 'More Information'
-    config.add_show_field 'language_ssim', label: 'Language'
-    config.add_show_field 'published_ssim', label: 'Published'
-    config.add_show_field 'published_vern_ssim', label: 'Published'
-    config.add_show_field 'lc_callnum_ssim', label: 'Call number'
-    config.add_show_field 'isbn_ssim', label: 'ISBN'
+
+    #{show}
+
 
     # "fielded" search configuration. Used by pulldown among other places.
     # For supported keys in hash, see rdoc for Blacklight::SearchFields
@@ -163,61 +169,152 @@ class CatalogController < ApplicationController
     # solr request handler? The one set in config[:default_solr_parameters][:qt],
     # since we aren't specifying it otherwise.
 
-    config.add_search_field 'all_fields', label: 'All Fields'
-
-
+    # NAHC Customizations to use existing "catchall" field called text
+    config.add_search_field 'text', label: 'Any Fields'
     # Now we see how to over-ride Solr request handler defaults, in this
     # case for a BL "search field", which is really a dismax aggregate
     # of Solr search fields.
 
-    config.add_search_field('title') do |field|
-      # solr_parameters hash are sent to Solr as ordinary url query params.
-      field.solr_parameters = {
-        'spellcheck.dictionary': 'title',
-        qf: '${title_qf}',
-        pf: '${title_pf}'
-      }
-    end
-
-    config.add_search_field('author') do |field|
-      field.solr_parameters = {
-        'spellcheck.dictionary': 'author',
-        qf: '${author_qf}',
-        pf: '${author_pf}'
-      }
-    end
-
-    # Specifying a :qt only to show it's possible, and so our internal automated
-    # tests can test it. In this case it's the same as
-    # config[:default_solr_parameters][:qt], so isn't actually neccesary.
-    config.add_search_field('subject') do |field|
-      field.qt = 'search'
-      field.solr_parameters = {
-        'spellcheck.dictionary': 'subject',
-        qf: '${subject_qf}',
-        pf: '${subject_pf}'
-      }
-    end
+    #   config.add_search_field('text') do |field|
+    #     # solr_parameters hash are sent to Solr as ordinary url query params.
+    #     field.solr_parameters = { :'spellciheck.dictionary' => 'text' }
+    #
+    #     # :solr_local_parameters will be sent using Solr LocalParams
+    #     # syntax, as eg {! qf=$title_qf }. This is neccesary to use
+    #     # Solr parameter de-referencing like $title_qf.
+    #     # See: http://wiki.apache.org/solr/LocalParams
+    #     field.solr_local_parameters = {
+    #       qf: '$text_qf',
+    #       pf: '$text_pf'
+    #     }
+    #   end
+    #
+    #    config.add_search_field('author') do |field|
+    #      field.solr_parameters = { :'spellcheck.dictionary' => 'author' }
+    #      field.solr_local_parameters = {
+    #        qf: '$author_qf',
+    #        pf: '$author_pf'
+    #      }
+    #    end
+    #
+    #    # Specifying a :qt only to show it's possible, and so our internal automated
+    #    # tests can test it. In this case it's the same as
+    #    # config[:default_solr_parameters][:qt], so isn't actually neccesary.
+    #    config.add_search_field('subject') do |field|
+    #      field.solr_parameters = { :'spellcheck.dictionary' => 'subject' }
+    #      field.qt = 'search'
+    #      field.solr_local_parameters = {
+    #        qf: '$subject_qf',
+    #        pf: '$subject_pf'
+    #   }
+    # end
 
     # "sort results by" select (pulldown)
-    # label in pulldown is followed by the name of the Solr field to sort by and
+    # label in pulldown is followed by the name of the SOLR field to sort by and
     # whether the sort is ascending or descending (it must be asc or desc
-    # except in the relevancy case). Add the sort: option to configure a
-    # custom Blacklight url parameter value separate from the Solr sort fields.
-    config.add_sort_field 'relevance', sort: 'score desc, pub_date_si desc, title_si asc', label: 'relevance'
-    config.add_sort_field 'year-desc', sort: 'pub_date_si desc, title_si asc', label: 'year'
-    config.add_sort_field 'author', sort: 'author_si asc, title_si asc', label: 'author'
-    config.add_sort_field 'title_si asc, pub_date_si desc', label: 'title'
+    # except in the relevancy case).
+
+    #{sort}
 
     # If there are more than this many search results, no spelling ("did you
     # mean") suggestion is offered.
     config.spell_max = 5
 
-    # Configuration for autocomplete suggester
+    # Configuration for autocomplete suggestor
     config.autocomplete_enabled = true
     config.autocomplete_path = 'suggest'
-    # if the name of the solr.SuggestComponent provided in your solrconfig.xml is not the
-    # default 'mySuggester', uncomment and provide it below
-    # config.autocomplete_suggester = 'mySuggester'
+
+    # sort
+    config.add_sort_field 'objname_s asc', label: 'Object name'
+    config.add_sort_field 'objsortnum_s asc', label: 'Object number'
+    config.index.title_field =  'objname_s'
+    config.show.title_field =  'objname_s'
+
+    # index
+    config.add_index_field 'institution_s', label: 'Institution'
+    config.add_index_field 'reported_s', label: 'Reported'
+    config.add_index_field 'inventory_name_s', label: 'Inventory Name'
+    config.add_index_field 'portal_posting_date_dt', label: 'Portal Posting Date'
+    config.add_index_field 'objmusno_s', label: 'Object Number'
+    config.add_index_field 'objtype_s', label: 'Type'
+    config.add_index_field 'objassoccult_ss', label: 'Culture'
+    config.add_index_field 'objfcp_s', label: 'Collection Place'
+    config.add_index_field 'status_ss', label: 'Status'
+
+    # search
+    [
+      ['objmusno_s', 'Object number'],
+      ['objname_txt', 'Object name'],
+      ['objdescr_txt', 'Description'],
+      ['objfcp_txt', 'Collection place'],
+      ['objassoccult_txt', 'Culture or time period']
+      ].each do |search_field|
+      config.add_search_field(search_field[0]) do |field|
+        field.label = search_field[1]
+        #field.solr_parameters = { :'spellcheck.dictionary' => search_field[0] }
+        field.solr_parameters = {
+          qf: search_field[0],
+          pf: search_field[0]
+        }
+      end
+    end
+
+    # grid
+    # config.add_grid_field 'institution_s', label: 'Institution'
+    # config.add_grid_field 'reported_s', label: 'Reported'
+    # config.add_grid_field 'inventory_name_s', label: 'Inventory Name'
+    # config.add_grid_field 'portal_posting_date_dt', label: 'Portal Posting Date'
+    # config.add_grid_field 'objmusno_s', label: 'Object Number'
+    # config.add_grid_field 'objtype_s', label: 'Type'
+
+    # show
+    config.add_show_field 'institution_s', label: 'Institution'
+    config.add_show_field 'reported_s', label: 'Reported'
+    config.add_show_field 'inventory_name_s', label: 'Inventory Name'
+    config.add_show_field 'submission_date_dt', label: 'Submission Date'
+    config.add_show_field 'received_date_dt', label: 'Received Date'
+    config.add_show_field 'entered_date_dt', label: 'Entered Date'
+    config.add_show_field 'portal_posting_date_dt', label: 'Portal Posting Date'
+    config.add_show_field 'objmusno_s', label: 'Object Number'
+    config.add_show_field 'objdept_s', label: 'Department'
+    config.add_show_field 'objtype_s', label: 'Type'
+    config.add_show_field 'objassoccult_ss', label: 'Culture'
+    config.add_show_field 'objfcp_s', label: 'Collection Place'
+    config.add_show_field 'status_ss', label: 'Status'
+
+    # facets
+    config.add_facet_field 'institution_s', label: 'Institution'
+    config.add_facet_field 'inventory_name_s', label: 'Inventory Name', limit: true, index_range: true
+    config.add_facet_field 'objname_s', label: 'Object Name', limit: true, index_range: true
+    config.add_facet_field 'submission_date_dt', label: 'Submission Date', limit: true, index_range: true
+    config.add_facet_field 'received_date_dt', label: 'Received Date', limit: true, index_range: true
+    config.add_facet_field 'entered_date_dt', label: 'Entered Date', limit: true, index_range: true
+    config.add_facet_field 'portal_posting_date_dt', label: 'Portal Posting Date', limit: true, index_range: true
+    config.add_facet_field 'objdept_s', label: 'Department', limit: true, index_range: true
+    config.add_facet_field 'objtype_s', label: 'Type', limit: true, index_range: true
+    config.add_facet_field 'objassoccult_ss', label: 'Culture', limit: true, index_range: true
+    config.add_facet_field 'objfcp_s', label: 'Collection Place', limit: true, index_range: true
+    config.add_facet_field 'reported_s', label: 'Reported'
+    config.add_facet_field 'status_ss', label: 'Status'
+
+    # gallery
   end
+
+  def decode_ark
+    # decode ARK ID, e.g. hm21114461@2E1 -> 11-4461.1, hm210k3711a@2Df -> K-3711a-f
+    museum_number = CGI.unescape(params[:ark].gsub('@','%')).sub('hm2','')
+    museum_number = if museum_number[0] == 'x'
+        museum_number[1..-1]
+    else
+        left, right = museum_number[1..2], museum_number[3..-1]
+        left = left.gsub(/^0+/, '')
+        right = right.gsub(/^0+/, '')
+        left + '-' + right
+    end
+
+    redirect_to  :controller => 'catalog', action: 'index', search_field: 'objmusno_s_lower', q: '"' + museum_number + '"'
+    #redirect_to  :controller => 'catalog', action: 'show', id: csid
+
+  end
+
 end
