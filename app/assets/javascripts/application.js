964b41c6296a0e826dde8b631190dfde3551c903
John Lowe <jblowe@berkeley.edu>
2021-03-22 16:07:04 -0700

    initial commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,22 @@
+//= require jquery
+//= require 'blacklight_advanced_search'
+
+
+//= require jquery3
+//= require rails-ujs
+//= require turbolinks
+//
+// Required by Blacklight
+//= require popper
+// Twitter Typeahead for autocomplete
+//= require twitter/typeahead
+//= require bootstrap
+//= require blacklight/blacklight
+//= require blacklight_gallery
+
+
+
+// For blacklight_range_limit built-in JS, if you don't want it you don't need
+// this:
+//= require 'blacklight_range_limit'
+
